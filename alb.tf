e04db531755e46222be8c2162d7d446428d93aa7
root <root@DESKTOP-Subash.localdomain>
2020-02-21 19:09:29 -0500

    f

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "default" {
   name               = "terraform-springboot-helloapp"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.default _lb.id}"]
+  security_groups    = ["${aws_security_group.default_lb.id}"]
   subnets            = ["subnet-9b6988ba", "subnet-6641eb2b"]
 
   enable_deletion_protection = true
