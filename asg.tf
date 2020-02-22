7f40d0602ae901b590dee9124659326f14b3debc
root <root@DESKTOP-Subash.localdomain>
2020-02-21 19:27:41 -0500

    .

--- asg.tf
+++ asg.tf
@@ -8,4 +8,6 @@ resource "aws_autoscaling_group" "default" {
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
   vpc_zone_identifier       = ["subnet-9b6988ba", "subnet-6641eb2b"]
+  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
+
 }
